7069fdb82f7ee7bd5729f9c6065ab61c0ed1ddae
synw <synwx@protonmail.com>
2023-08-21 16:40:30 +0200

    Add more examples

---
+++ examples/js/sse/restmix_lib.js
@@ -0,0 +1,60 @@
+#!/usr/bin/env node
+const { useApi } = require("restmix");
+
+// doc: https://synw.github.io/restmix/ts/postsse
+
+const apiKey = "7aea109636aefb984b13f9b6927cd174425a1e05ab5f2e3935ddfeb183099465";
+const model = "mamba-gpt-3b-v3.ggmlv3.q8_0";
+const template = "### Instruction: {prompt}\n\n### Response:";
+const prompt = "List the planets in the solar system";
+
+const api = useApi({ "serverUrl": "http://localhost:5143" });
+api.addHeader('Authorization', `Bearer ${apiKey}`);
+
+async function loadModel() {
+  const res = await api.post("/model/load", {
+    model: model
+  });
+  if (!res.ok) {
+    throw new Error("Can not load model", res)
+  }
+}
+
+async function runInference() {
+  process.stdout.setEncoding('utf8');
+  const onChunk = (payload) => {
+    switch (payload.msg_type) {
+      case "token":
+        process.stdout.write(payload.content)
+        break;
+      case "system":
+        console.log("\nSystem msg:", payload);
+      default:
+        break;
+    }
+  };
+  const abortController = new AbortController();
+  const _payload = {
+    prompt: prompt,
+    template: template,
+    stream: true,
+    temperature: 0.6,
+  };
+  await api.postSse(
+    "/completion",
+    _payload,
+    onChunk,
+    abortController,
+    false,
+    true,
+  );
+}
+
+async function main() {
+  await loadModel();
+  await runInference();
+}
+
+(async () => {
+  await main();
+})();
---
+++ examples/python/sse.py
@@ -0,0 +1,44 @@
+import json
+import sys
+import sseclient
+import requests
+
+KEY = "7aea109636aefb984b13f9b6927cd174425a1e05ab5f2e3935ddfeb183099465"
+URL = "https://152334h.github.io/blog/non-determinism-in-gpt-4/"
+MODEL = "mamba-gpt-3b-v3.ggmlv3.q8_0"
+TEMPLATE = "### Instruction: {prompt}\n\n### Response:"
+PROMPT = "list the planets in the solar system"
+
+# load a language model
+payload = {
+   "model": MODEL,
+   "ctx": 4096,
+}
+headers = {'Authorization': f'Bearer {KEY}'}
+url = 'http://localhost:5143/model/load'
+response = requests.post(url, headers=headers, json=payload)
+if (response.status_code != 204):
+  raise Exception(f"Error loading model ${response.content}")
+
+# run the inference query
+payload = {
+   "prompt": PROMPT,
+   "template": TEMPLATE,
+   "stream": True,
+   "temperature": 0.6,
+}
+headers['Accept'] = 'text/event-stream'
+url = 'http://localhost:5143/completion'
+response = requests.post(url, stream=True, headers=headers, json=payload)
+client = sseclient.SSEClient(response)
+for event in client.events():
+    data = json.loads(event.data)
+    if data["msg_type"] == "token":
+      print(data["content"], end='', flush=True)
+    elif data["msg_type"] == "system":
+      if data["content"] == "result":
+        print("\n\nRESULT:")
+        print(data)
+      else:
+        print("SYSTEM:", data, "\n")
+    
