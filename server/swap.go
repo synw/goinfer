199710b8444b61c4a2c0716de97b42b375162379
Oliver <oliver@hessling.fr>
2025-08-23 22:51:00 +0200

    Make srv variable same type as the returned type

--- server/swap.go
+++ server/swap.go
@@ -12,11 +12,11 @@ func NewProxyServer(cfg conf.GoInferConf) (*http.Server, *proxy.ProxyManager) {
 	for addr, services := range cfg.Server.Listen {
 		if strings.Contains(services, "swap") {
 			pm := proxy.New(cfg.Proxy)
-			srv := http.Server{
+			srv := &http.Server{
 				Addr:    addr,
 				Handler: pm,
 			}
-			return &srv, pm
+			return srv, pm
 		}
 	}
 	return nil, nil // llama-swap not present => not enabled
